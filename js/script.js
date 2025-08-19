4bb230fce072f369148d2c603317d9def1dd070e
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2025-08-19 20:33:40 -0300

    Script para adicionar tarefas pronto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-console.log('Início do script')
+const button = document.querySelector('.button-add-tarefa')
+
+const input = document.querySelector('.input-tarefa')
+
+const minhaLista = []
+
+function adicionarNovaTarefa() {
+    minhaLista.push(input.value)
+
+    mostrarTarefas()
+
+}
+
+function mostrarTarefas() {
+
+    const ul = document.querySelector('ul')
+
+    ul.innerHTML = ""
+
+    minhaLista.forEach(tarefa => {
+
+        let elmTarefa = document.createElement('p')
+        elmTarefa.innerHTML = tarefa
+
+        const novaLi = document.createElement('li')
+        novaLi.classList.add('tarefa')
+        
+        const imgCheck = document.createElement('img')
+        imgCheck.src = "./img/checked.png"
+
+        const imgTrash = document.createElement('img')
+        imgTrash.src = "./img/trash.png"
+
+        novaLi.appendChild(imgCheck)
+        novaLi.appendChild(elmTarefa)
+        novaLi.appendChild(imgTrash)
+
+        ul.appendChild(novaLi)
+
+    })
+
+            // <li class="tarefa">
+            //     <img src="./img/checked.png" alt="Check na tarefa">
+            //     <p>Tarefa número um</p>
+            //     <img src="./img/trash.png" alt="Exclui tarefa">
+            // </li>
+
+    console.log(minhaLista)
+}
+
+button.addEventListener('click', adicionarNovaTarefa)

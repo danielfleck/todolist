1bded0a847e82cba43aff46793e1af6979840a3f
Daniel Rodrigo Fleck <danielfleck@gmail.com>
2025-08-19 21:37:47 -0300

    Projeto concluído

--- js/script.js
+++ js/script.js
@@ -2,13 +2,32 @@ const button = document.querySelector('.button-add-tarefa')
 
 const input = document.querySelector('.input-tarefa')
 
-const minhaLista = []
+let minhaLista = []
 
 function adicionarNovaTarefa() {
-    minhaLista.push(input.value)
+    minhaLista.push(
+        { tarefa: input.value , concluida: false }
+    )
 
     mostrarTarefas()
 
+}
+
+function concluirTarefa(e) {
+
+    const li = e.target.parentElement
+    
+    const posicao = e.target.parentElement.posicao
+
+    minhaLista[li.posicao].concluida = !minhaLista[li.posicao].concluida    
+
+    mostrarTarefas() 
+}
+
+function deletarItem(e) {
+    minhaLista.splice(e.target.parentElement.posicao,1)
+
+    mostrarTarefas() 
 }
 
 function mostrarTarefas() {
@@ -17,35 +36,39 @@ function mostrarTarefas() {
 
     ul.innerHTML = ""
 
-    minhaLista.forEach(tarefa => {
+    minhaLista.forEach( (tarefa, posicao) => {
 
         let elmTarefa = document.createElement('p')
-        elmTarefa.innerHTML = tarefa
+        elmTarefa.innerHTML = tarefa.tarefa
+
 
         const novaLi = document.createElement('li')
         novaLi.classList.add('tarefa')
+        novaLi.posicao = posicao
         
         const imgCheck = document.createElement('img')
         imgCheck.src = "./img/checked.png"
-
+        imgCheck.addEventListener('click', concluirTarefa)
+        
         const imgTrash = document.createElement('img')
         imgTrash.src = "./img/trash.png"
+        imgTrash.addEventListener('click', deletarItem)
 
         novaLi.appendChild(imgCheck)
         novaLi.appendChild(elmTarefa)
         novaLi.appendChild(imgTrash)
 
+
+        if(tarefa.concluida) {
+            novaLi.classList.add('tarefa-concluida')   
+        } else {
+            novaLi.classList.remove('tarefa-concluida') 
+        }
+
         ul.appendChild(novaLi)
 
     })
 
-            // <li class="tarefa">
-            //     <img src="./img/checked.png" alt="Check na tarefa">
-            //     <p>Tarefa número um</p>
-            //     <img src="./img/trash.png" alt="Exclui tarefa">
-            // </li>
-
-    console.log(minhaLista)
 }
 
 button.addEventListener('click', adicionarNovaTarefa)
